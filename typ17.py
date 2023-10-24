579e65a778c2ead4616cfddf6bb93b1f4bc3ae1e
Naiitiry <danchukroman@gmail.com>
2023-10-24 00:14:21 -0300

    teoria 17

---
+++ typ17.py
@@ -0,0 +1,5 @@
+#  ------------------------- Programación Modular -------------------------
+
+# Es la división, de un programa grande, en subprograma/módulos.
+# Mejora el manejo del código  y de su lectura,
+# 
