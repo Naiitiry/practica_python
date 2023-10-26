38f26f5fb2fb9d124927ad94103c124ea4280f49
Naiitiry <you@example.com>
2023-10-26 10:29:48 -0300

    modulo

--- typ17.py
+++ typ17.py
@@ -2,4 +2,8 @@
 
 # Es la división, de un programa grande, en subprograma/módulos.
 # Mejora el manejo del código  y de su lectura,
-# 
+def pow(num1,num2):
+    return num1**num2
+
+calculo_1 = pow(2,7)
+print(calculo_1)
