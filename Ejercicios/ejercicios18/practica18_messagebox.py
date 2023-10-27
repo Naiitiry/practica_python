0ba1e9fab5f059b8966f43020bfcdc60288035df
Naiitiry <you@example.com>
2023-10-27 14:23:54 -0300

    cambiar valor de messagebox

--- Ejercicios/ejercicios18/practica18_messagebox.py
+++ Ejercicios/ejercicios18/practica18_messagebox.py
@@ -1,6 +1,12 @@
 #importaciones
 import tkinter.messagebox as msb
+# nos permite guardar, en una variable, el resultado queda en consola, dependiendo de la alerta cambia el aviso.
+# valor = msb.showinfo("Hola bo","Probando en consola el messagebox")
 
-valor = msb.showinfo("Hola bo","Probando en consola el messagebox")
-
-print(valor)
+valor2 = msb.askyesnocancel("Hola bo","Probando en consola el messagebox")
+if valor2:
+    print("El usuario ha pulsado el botón 'Sí'.")
+elif valor2 is False:
+    print("El usuario ha pulsado el botón 'No'.")
+else:
+    print("El usuario ha pulsado el botón 'Cancelar'.")
